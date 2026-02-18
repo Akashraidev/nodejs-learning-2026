4f6666884e768124375369669cd48894435a87d9
Akash Rai <akashrai4910@gmail.com>
2026-02-18 23:56:00 +0530

    Structured routes using app.route for cleaner code.

--- project-01/index.js
+++ project-01/index.js
@@ -7,24 +7,46 @@ const port = 8000;
 
 /// Routes
 
-app.get("/api/user",(req,res) => {
-    return res.json(user);
-});
+// app.get("/api/user",(req,res) => {
+//     return res.json(user);
+// });
 
-app.get("/user", (req, res) => {
-  const html = `
-    <ul>
-      ${user.map(u => `<li>${u.first_name}</li>`).join("")}
-    </ul>
-  `;
-  res.send(html);
-});
+// app.get("/user", (req, res) => {
+//   const html = `
+//     <ul>
+//       ${user.map(u => `<li>${u.first_name}</li>`).join("")}
+//     </ul>
+//   `;
+//   res.send(html);
+// });
 
-app.get("/api/user/:id",(req,res)=>{
-    const id = Number(req.params.id);
-    const us = user.find((u)=> u.id === id);
-    return res.json(us);
-});
+// app.get("/api/user/:id",(req,res)=>{
+//     const id = Number(req.params.id);
+//     const us = user.find((u)=> u.id === id);
+//     return res.json(us);
+// });
+
+// app.post("/api/user",(req,res)=>{
+//     return res.json({status: "pending"});
+// })
+
+// app.patch("/api/user/:id",(req,res)=>{
+//     return res.json({status : "Pending"})
+// })
 
 
-app.listen(port,()=> console.log(`Server Started at Port: ${port}`));
+app.route("/api/user/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const us = user.find((u) => u.id === id);
+        return res.json(us);
+    })
+    .put((req,res)=>{
+        //Edit
+         return res.json({status : "Pending"})
+    })
+    .delete((req,res =>{
+         return res.json({status : "Pending"})
+    }))
+
+app.listen(port, () => console.log(`Server Started at Port: ${port}`));
